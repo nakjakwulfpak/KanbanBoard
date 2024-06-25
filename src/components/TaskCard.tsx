16830f313f997b1fd1831175dc99c9462374495b
Dirtcube Website <nakul11999@gmail.com>
2024-06-25 18:45:07 +0530

    responsiveness added

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -19,8 +19,8 @@ const TaskCard = ({ task,updateTaskPoints,updateTaskTitle }: { task: Task, updat
         onDragStart={(e)=>{
             e.dataTransfer.setData('id',task.id)
         }}
-        className="flex flex-col bg-gray-200 justify-start gap-y-4 ml-4 items-start p-4 border border-black  rounded-lg mt-4  w-[450px]">
-      <div className="text-xl text-black font-bold leading-[150%] ">
+        className="flex flex-col bg-gray-200 justify-between gap-y-4 ml-4 items-start p-4 border border-black  rounded-lg mt-4  w-full h-full">
+      <div className="text-xl text-black font-bold leading-[150%] w-full ">
       {isEditing ?(
             <input autoFocus
                 type="text"
@@ -34,10 +34,10 @@ const TaskCard = ({ task,updateTaskPoints,updateTaskTitle }: { task: Task, updat
         </div>
         )}
       </div>
-      <div className="flex justify-between items-center w-full">
+      <div className="flex justify-between max-md:gap-x-14 gap-x-40 items-center w-full">
         <p className="text-lg font-semibold text-gray-600">{task.id}</p>
         <p className="text-lg font-semibold text-gray-600">{task.priority}</p>
-        <div className="flex items-center gap-x-2">
+        <div className="flex items-center gap-x-2 ">
             <button className="text-black" onClick={()=>updatePoints('up')}>+</button>
             <p className="text-lg text-gray-600">{points}</p>
             <button className="text-black" onClick={()=>updatePoints('down')}>-</button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,16 @@ import Image from "next/image";
 import { useState } from "react";
 import { Inter } from "next/font/google";
 import TaskCard from "@/components/TaskCard";
-import { tasks as initialTasks, statuses,Task,Status } from "@/utils/data-task";
+import {
+  tasks as initialTasks,
+  statuses,
+  Task,
+  Status,
+} from "@/utils/data-task";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [tasks,setTasks]=useState<Task[]>(initialTasks)
+  const [tasks, setTasks] = useState<Task[]>(initialTasks);
   const columns = statuses.map((status) => {
     const tasksInColumn = tasks.filter((task) => task.status === status);
     return {
@@ -14,53 +19,70 @@ export default function Home() {
       tasks: tasksInColumn,
     };
   });
-  const updateTaskPoints=(task:Task,points:number)=>{
-    const updatedTask={...task,points:points}
-    const updatedTasks=tasks.map((t)=>t.id===task.id?updatedTask:t)
-    setTasks(updatedTasks)
-  }
-  const updateTaskTitle=(task:Task,title:string)=>{
-    const updatedTask={...task,title:title}
-    const updatedTasks=tasks.map((t)=>t.id===task.id?updatedTask:t)
-    setTasks(updatedTasks)
-  }
-  const updateTaskStatus=(task:Task,status:Status)=>{
-    const updatedTask={...task,status:status}
-    const updatedTasks=tasks.map((t)=>t.id===task.id?updatedTask:t)
-    setTasks(updatedTasks)
-  }
+  const updateTaskPoints = (task: Task, points: number) => {
+    const updatedTask = { ...task, points: points };
+    const updatedTasks = tasks.map((t) => (t.id === task.id ? updatedTask : t));
+    setTasks(updatedTasks);
+  };
+  const updateTaskTitle = (task: Task, title: string) => {
+    const updatedTask = { ...task, title: title };
+    const updatedTasks = tasks.map((t) => (t.id === task.id ? updatedTask : t));
+    setTasks(updatedTasks);
+  };
+  const updateTaskStatus = (task: Task, status: Status) => {
+    const updatedTask = { ...task, status: status };
+    const updatedTasks = tasks.map((t) => (t.id === task.id ? updatedTask : t));
+    setTasks(updatedTasks);
+  };
 
-  const handleDrop=(e:React.DragEvent<HTMLDivElement>,status:Status)=>{
-    const id=e.dataTransfer.getData('id')
-    setCurrentlyHoveringOver(null)
-    const task=tasks.find((t)=>t.id===id)
-    if(task){
-      updateTaskStatus(task,status)
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>, status: Status) => {
+    const id = e.dataTransfer.getData("id");
+    setCurrentlyHoveringOver(null);
+    const task = tasks.find((t) => t.id === id);
+    if (task) {
+      updateTaskStatus(task, status);
     }
-  }
-  const[currentlyHoveringOver,setCurrentlyHoveringOver]=useState<Status|null>(null)
-  const handleDragEnter=(status:Status)=>{
-    setCurrentlyHoveringOver(status)
-    console.log('drag enter',status)
-  }
+  };
+  const [currentlyHoveringOver, setCurrentlyHoveringOver] =
+    useState<Status | null>(null);
+  const handleDragEnter = (status: Status) => {
+    setCurrentlyHoveringOver(status);
+    console.log("drag enter", status);
+  };
   return (
-    <main className=" bg-white  w-screen h-screen">
-      <div className="flex divide-x divide-black items-start ml-2 gap-x-20 w-full h-full">
+    <main className="w-screen h-screen overflow-y-scroll">
+      <div className="flex items-start justify-start gap-x-20 w-full h-full max-lg:flex-wrap max-lg:justify-center max-lg:gap-y-10 max-lg:items-center">
         {columns.map((column) => (
-          <div key={column.status} 
-            onDrop={(e)=>handleDrop(e,column.status)} 
-            onDragOver={(e)=>e.preventDefault()}
-            onDragEnter={(e)=>handleDragEnter(column.status)} > 
-            <div className="flex justify-between items-center">
-              <p className="text-3xl p-2 font-bol capitalize text-gray-500">{column.status}</p>
-              <p className="text-3xl text-gray-600">{column.tasks.reduce((total,task)=>total+(task?.points || 0),0)}</p>
+          <div
+            key={column.status}
+            onDrop={(e) => handleDrop(e, column.status)}
+            onDragOver={(e) => e.preventDefault()}
+            onDragEnter={(e) => handleDragEnter(column.status)}
+            className=""
+          >
+            <div className="flex justify-between max-md:justify-start items-start w-full h-full">
+              <p className="text-3xl font-bol max-md:ml-0 ml-4 capitalize text-gray-500">
+                {column.status}
+              </p>
+              <p className="text-3xl text-gray-600">
+                {column.tasks.reduce(
+                  (total, task) => total + (task?.points || 0),
+                  0
+                )}
+              </p>
             </div>
-            <div className={`${currentlyHoveringOver===column.status?'bg-gray-200 w-full h-full':''}`}>
+            <div
+              className={`${
+                currentlyHoveringOver === column.status
+                  ? "bg-gray-200 w-full h-full "
+                  : ""
+              }`}
+            >
               {column.tasks.map((task) => (
-                <TaskCard 
-                  key={task.id} 
+                <TaskCard
+                  key={task.id}
                   task={task}
-                  updateTaskPoints={updateTaskPoints} 
+                  updateTaskPoints={updateTaskPoints}
                   updateTaskTitle={updateTaskTitle}
                 />
               ))}
